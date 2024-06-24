735f7562df743cdb20c1431c3a2667330c948093
chioma-okeke <okekegeraldenne@gmail.com>
2024-06-24 11:21:46 +0100

    modified UI and added redirects folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,18 +25,18 @@ function Home() {
                     alt=""
                     className="w-[30%] sm:w-[20%] md:w-[20%] my-3 lg:my-2 transition ease-out hover:rotate-6 hover:scale-110 duration-300"
                 />
-                <h1 className="text-3xl sm:text-6xl font-bold mb-20">
+                <h1 className="text-2xl sm:text-4xl font-bold mb-20">
                     Tic-Tac-Toe Time!
                 </h1>
                 <button
-                    className="w-full lg:w-[80%] text-lg lg:text-2xl py-4 px-2.5 rounded-2xl flex items-center bg-white shadow-md mb-5 transition ease-in-out hover:bg-[#9a0001] focus:bg-[#9a0001] hover:text-[#fff] focus:text-[#fff] duration-500"
+                    className="w-full md:w-[80%] text-lg lg:text-2xl py-4 px-2.5 rounded-2xl flex items-center bg-white shadow-md mb-5 transition ease-in-out hover:bg-[#9a0001] focus:bg-[#9a0001] hover:text-[#fff] focus:text-[#fff] duration-500"
                     onClick={() => handlePlayMode("play-with-computer")}
                 >
                     <IoPersonSharp size={25} className="w-[10%]" />
                     <span className="w-[90%]">Play Solo</span>
                 </button>
                 <button
-                    className="w-full lg:w-[80%] text-lg lg:text-2xl py-4 px-2.5 rounded-2xl flex items-center bg-white shadow-md mb-10 transition ease-in-out hover:bg-[#9a0001] focus:bg-[#9a0001] hover:text-[#fff] focus:text-[#fff] duration-500"
+                    className="w-full md:w-[80%] text-lg lg:text-2xl py-4 px-2.5 rounded-2xl flex items-center bg-white shadow-md mb-10 transition ease-in-out hover:bg-[#9a0001] focus:bg-[#9a0001] hover:text-[#fff] focus:text-[#fff] duration-500"
                     onClick={() => handlePlayMode("play-with-opponent")}
                 >
                     <IoPeople size={25} className="w-[10%]" />
@@ -45,9 +45,9 @@ function Home() {
                 <Link
                     to="/gameScreen"
                     state={{ selectedValue: selectedValue }}
-                    className="w-full lg:w-[80%] flex items-center justify-center rounded-2xl mt-5 bg-gradient-to-r from-[#01d0fa] to-[#9a0001] p-2 transition ease-out hover:bg-white hover:text-[#01d0fa] hover:scale-110 duration-300"
+                    className="w-full md:w-[80%] flex items-center justify-center rounded-2xl mt-5 bg-gradient-to-r from-[#01d0fa] to-[#9a0001] p-2 transition ease-out hover:bg-white hover:text-[#01d0fa] hover:scale-110 duration-300"
                 >
-                    <PiPlayFill color="white" size={50} />
+                    <PiPlayFill color="white" size={45} />
                 </Link>
                 {/* {showGameSettings && (
                 <GameSettings
